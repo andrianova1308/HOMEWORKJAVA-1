8d9a1a73f71f0eb585c3b7c511252bb2dd86ce77
Анастасия Самсикова <anastasiasamsikova@192.168.1.4>
2021-11-25 23:23:03 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int sum_transfer = 500000000;
+        int total = balance + sum_transfer;
+        System.out.println(total);
+    }
+}
